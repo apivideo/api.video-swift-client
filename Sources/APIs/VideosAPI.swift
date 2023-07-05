a0329c79cbee2e8e84f0a1f8cadc7b0ca9ab6d5f
ThibaultBee <ThibaultBee@users.noreply.github.com>
2023-07-05 14:33:25 +0000

    Update parameter descriptions and add enums

--- Sources/APIs/VideosAPI.swift
+++ Sources/APIs/VideosAPI.swift
@@ -599,22 +599,40 @@ NOTE: If you are updating an array, you must provide the entire array as what yo
 
 
     /**
+     * enum for parameter sortBy
+     */
+    public enum SortBy_list: String, CaseIterable {
+        case title = "title"
+        case createdat = "createdAt"
+        case publishedat = "publishedAt"
+        case updatedat = "updatedAt"
+    }
+
+    /**
+     * enum for parameter sortOrder
+     */
+    public enum SortOrder_list: String, CaseIterable {
+        case asc = "asc"
+        case desc = "desc"
+    }
+
+    /**
      List all video objects
      
      - parameter title: (query) The title of a specific video you want to find. The search will match exactly to what term you provide and return any videos that contain the same term as part of their titles. (optional)
      - parameter tags: (query) A tag is a category you create and apply to videos. You can search for videos with particular tags by listing one or more here. Only videos that have all the tags you list will be returned. (optional)
      - parameter metadata: (query) Videos can be tagged with metadata tags in key:value pairs. You can search for videos with specific key value pairs using this parameter. [Dynamic Metadata](https://api.video/blog/endpoints/dynamic-metadata) allows you to define a key that allows any value pair. (optional)
-     - parameter description: (query) If you described a video with a term or sentence, you can add it here to return videos containing this string. (optional)
-     - parameter liveStreamId: (query) If you know the ID for a live stream, you can retrieve the stream by adding the ID for it here. (optional)
-     - parameter sortBy: (query) Allowed: publishedAt, title. You can search by the time videos were published at, or by title. (optional)
-     - parameter sortOrder: (query) Allowed: asc, desc. asc is ascending and sorts from A to Z. desc is descending and sorts from Z to A. (optional)
+     - parameter description: (query) Retrieve video objects by &#x60;description&#x60;. (optional)
+     - parameter liveStreamId: (query) Retrieve video objects that were recorded from a live stream by &#x60;liveStreamId&#x60;. (optional)
+     - parameter sortBy: (query) Use this parameter to sort videos by the their created time, published time, updated time, or by title. (optional)
+     - parameter sortOrder: (query) Use this parameter to sort results. &#x60;asc&#x60; is ascending and sorts from A to Z. &#x60;desc&#x60; is descending and sorts from Z to A. (optional)
      - parameter currentPage: (query) Choose the number of search results to return per page. Minimum value: 1 (optional, default to 1)
      - parameter pageSize: (query) Results per page. Allowed values 1-100, default is 25. (optional, default to 25)
      - parameter apiResponseQueue: The queue on which api response is dispatched.
      - parameter completion: completion handler to receive the data and the error objects.
      */
     @discardableResult
-    open class func list(title: String? = nil, tags: [String]? = nil, metadata: [String: String]? = nil, description: String? = nil, liveStreamId: String? = nil, sortBy: String? = nil, sortOrder: String? = nil, currentPage: Int? = nil, pageSize: Int? = nil, apiResponseQueue: DispatchQueue = ApiVideoClient.apiResponseQueue, completion: @escaping ((_ data: VideosListResponse?, _ error: Error?) -> Void)) -> RequestTask {
+    open class func list(title: String? = nil, tags: [String]? = nil, metadata: [String: String]? = nil, description: String? = nil, liveStreamId: String? = nil, sortBy: SortBy_list? = nil, sortOrder: SortOrder_list? = nil, currentPage: Int? = nil, pageSize: Int? = nil, apiResponseQueue: DispatchQueue = ApiVideoClient.apiResponseQueue, completion: @escaping ((_ data: VideosListResponse?, _ error: Error?) -> Void)) -> RequestTask {
             return listWithRequestBuilder(title: title, tags: tags, metadata: metadata, description: description, liveStreamId: liveStreamId, sortBy: sortBy, sortOrder: sortOrder, currentPage: currentPage, pageSize: pageSize).execute(apiResponseQueue) { result in
                 switch result {
                 case let .success(response):
@@ -633,15 +651,15 @@ NOTE: If you are updating an array, you must provide the entire array as what yo
      - parameter title: (query) The title of a specific video you want to find. The search will match exactly to what term you provide and return any videos that contain the same term as part of their titles. (optional)
      - parameter tags: (query) A tag is a category you create and apply to videos. You can search for videos with particular tags by listing one or more here. Only videos that have all the tags you list will be returned. (optional)
      - parameter metadata: (query) Videos can be tagged with metadata tags in key:value pairs. You can search for videos with specific key value pairs using this parameter. [Dynamic Metadata](https://api.video/blog/endpoints/dynamic-metadata) allows you to define a key that allows any value pair. (optional)
-     - parameter description: (query) If you described a video with a term or sentence, you can add it here to return videos containing this string. (optional)
-     - parameter liveStreamId: (query) If you know the ID for a live stream, you can retrieve the stream by adding the ID for it here. (optional)
-     - parameter sortBy: (query) Allowed: publishedAt, title. You can search by the time videos were published at, or by title. (optional)
-     - parameter sortOrder: (query) Allowed: asc, desc. asc is ascending and sorts from A to Z. desc is descending and sorts from Z to A. (optional)
+     - parameter description: (query) Retrieve video objects by &#x60;description&#x60;. (optional)
+     - parameter liveStreamId: (query) Retrieve video objects that were recorded from a live stream by &#x60;liveStreamId&#x60;. (optional)
+     - parameter sortBy: (query) Use this parameter to sort videos by the their created time, published time, updated time, or by title. (optional)
+     - parameter sortOrder: (query) Use this parameter to sort results. &#x60;asc&#x60; is ascending and sorts from A to Z. &#x60;desc&#x60; is descending and sorts from Z to A. (optional)
      - parameter currentPage: (query) Choose the number of search results to return per page. Minimum value: 1 (optional, default to 1)
      - parameter pageSize: (query) Results per page. Allowed values 1-100, default is 25. (optional, default to 25)
      - returns: RequestBuilder<VideosListResponse> 
      */
-    open class func listWithRequestBuilder(title: String? = nil, tags: [String]? = nil, metadata: [String: String]? = nil, description: String? = nil, liveStreamId: String? = nil, sortBy: String? = nil, sortOrder: String? = nil, currentPage: Int? = nil, pageSize: Int? = nil) -> RequestBuilder<VideosListResponse> {
+    open class func listWithRequestBuilder(title: String? = nil, tags: [String]? = nil, metadata: [String: String]? = nil, description: String? = nil, liveStreamId: String? = nil, sortBy: SortBy_list? = nil, sortOrder: SortOrder_list? = nil, currentPage: Int? = nil, pageSize: Int? = nil) -> RequestBuilder<VideosListResponse> {
         let localVariablePath = "/videos"
         let localVariableURLString = ApiVideoClient.basePath + localVariablePath
         let localVariableParameters: [String: Any]? = nil
