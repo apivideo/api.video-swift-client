0bc0eb271ef9a5baf7c72c08127d3900f73e5572
szekelyzol <szekelyzol@users.noreply.github.com>
2023-10-15 18:34:07 +0000

    Update video-clip schema with descriptions

--- Sources/Models/VideoClip.swift
+++ Sources/Models/VideoClip.swift
@@ -10,9 +10,12 @@ import Foundation
 import AnyCodable
 #endif
 
+/** Use this object to create a smaller clip from a video you upload.  - You can only create video clips in the same request where you create the video container. - You cannot update the starting or ending timestamps of a video clip after you created the video container. - When you upload a video file into a container where you defined a starting and ending timestamp, the API trims the video according to those timestamps to create a clip. */
 public struct VideoClip: Codable, Hashable {
 
+    /** The timestamp that defines the beginning of the video clip you want to create. The value must follow the `HH:MM:SS` format. */
     public var startTimecode: String?
+    /** The timestamp that defines the end of the video clip you want to create. The value must follow the `HH:MM:SS` format. */
     public var endTimecode: String?
 
     public init(startTimecode: String? = nil, endTimecode: String? = nil) {
